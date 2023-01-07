77a843e1bd59083602f189902bd0531dd3bbb34f
Artem <updatethatpl0x@gmail.com>
2023-01-07 11:13:23 -0500

    updated styling of inputfield

--- nextapp/components/inputField.tsx
+++ nextapp/components/inputField.tsx
@@ -27,7 +27,7 @@ export default function InputField(props: InputProps) {
       <label htmlFor={`input-group-${props.label.toLowerCase()}`} className="block mb-[1px] text-sm font-medium text-gray-900 dark:text-gray-300">
         {props.label}
       </label>
-      <div className={props.withUnit ? "flex" : "relative mb-6"}>
+      <div className={props.withUnit ? "flex" : "relative"}>
         {props.withUnit ? (
           <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">{props.withUnit}</span>
         ) : (
