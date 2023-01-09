de29e50a13ef1d4ea8fb8a7c29fb8470bc30e719
Artem <updatethatpl0x@gmail.com>
2023-01-09 10:12:00 -0500

    fixed up some styling

--- nextapp/pages/api/get_pdf.ts
+++ nextapp/pages/api/get_pdf.ts
@@ -29,13 +29,17 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
   doc
     .font("Times-Bold")
     .fontSize(12)
-    .text(data.fields.name, 70 + (hasImage ? 80 : 0), 89);
+    .text(data.fields.name, 70 + (hasImage ? 80 : 0), 89)
+    .font("Times-Roman");
 
   if (data.fields.userAddress) {
     doc
       .font("Times-Roman")
       .fontSize(11)
-      .text(data.fields.userAddress, 70 + (hasImage ? 80 : 0), 110, { width: 120 });
+      .text(data.fields.userAddress, 70 + (hasImage ? 80 : 0), 110, { width: 140 });
+  }
+  if (data.fields.phone) {
+    doc.text(data.fields.phone, 70 + (hasImage ? 80 : 0), undefined, { width: 140 });
   }
   if (data.fields.number) {
     doc.text("Invoice #" + data.fields.number, doc.page.width - 120 - 70, 90, { width: 120, align: "center" });
@@ -44,8 +48,14 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
   date.setUTCFullYear(data.fields.date.split("-")[0], parseInt(data.fields.date.split("-")[1]) - 1, data.fields.date.split("-")[2]);
   doc.text(date.toLocaleDateString("en", { month: "long", day: "numeric", year: "numeric" }), doc.page.width - 120 - 70, data.fields.number ? 110 : 90, { width: 120, align: "center" });
 
+  if (data.fields.destName || data.fields.destAddress) {
+    doc.text("To: ", 70 + (hasImage ? 80 : 0), 165, { width: 140 });
+  }
+  if (data.fields.destName) {
+    doc.text(data.fields.destName, 70 + (hasImage ? 80 : 0), undefined, { width: 140 });
+  }
   if (data.fields.destAddress) {
-    doc.text("To: " + data.fields.destAddress, 70 + (hasImage ? 80 : 0), 165, { width: 120 });
+    doc.text(data.fields.destAddress, 70 + (hasImage ? 80 : 0), undefined, { width: 140 });
   }
   (async function () {
     // table
@@ -67,8 +77,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 
       datas: data.itemRows
         .map((row: ItemRow, i: number) => ({
-          name: "   " + row.name,
-          price: "   " + (isNaN(parseFloat(row.price)) ? row.price : "$" + row.price),
+          name: "   " + (row.name === undefined ? "" : row.name),
+          price: "   " + (isNaN(parseFloat(row.price)) ? (row.price === undefined ? "" : row.price) : "$" + row.price),
         }))
         .concat([
           { name: "", price: "" },
