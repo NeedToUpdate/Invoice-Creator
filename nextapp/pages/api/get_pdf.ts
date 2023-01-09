2fea3a430f3e73d0cd900d946581aaf99c2c6d3c
Artem <updatethatpl0x@gmail.com>
2023-01-09 09:15:02 -0500

    finalized pdf creation script

--- nextapp/pages/api/get_pdf.ts
+++ nextapp/pages/api/get_pdf.ts
@@ -1,9 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import PDFDocument from "pdfkit-table";
+import type PDFDocumentWithTables from "pdfkit-table";
 type Data = {
   name: string;
 };
+interface ItemRow {
+  name: string;
+  price: string;
+}
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const doc = new PDFDocument();
@@ -40,28 +45,82 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
   doc.text(date.toLocaleDateString("en", { month: "long", day: "numeric", year: "numeric" }), doc.page.width - 120 - 70, data.fields.number ? 110 : 90, { width: 120, align: "center" });
 
   if (data.fields.destAddress) {
-    doc.text("To: " + data.fields.destAddress, 70 + (hasImage ? 80 : 0), 160, { width: 120 });
+    doc.text("To: " + data.fields.destAddress, 70 + (hasImage ? 80 : 0), 165, { width: 120 });
   }
   (async function () {
     // table
     const table = {
-      headers: ["Description", "Amount"],
-      rows: data.itemRows
-        .map((row: { name: string; price: string }) => {
-          return [row.name, "$" + row.price];
-        })
-        .concat(Array(100 - data.itemRows.length).fill(["", ""]))
-        .concat([["Total", "$1000"]]),
+      headers: [
+        {
+          label: "   Description",
+          property: "name",
+          renderer: (value: string) => value,
+          width: doc.page.width - 240,
+        },
+        {
+          label: "   Amount",
+          property: "price",
+          renderer: (value: string) => value,
+          width: 100,
+        },
+      ],
+
+      datas: data.itemRows
+        .map((row: ItemRow, i: number) => ({
+          name: "   " + row.name,
+          price: "   " + (isNaN(parseFloat(row.price)) ? row.price : "$" + row.price),
+        }))
+        .concat([
+          { name: "", price: "" },
+          {
+            name: "bold:   Total",
+            price:
+              "bold:   $" +
+              data.itemRows.reduce((a: number, b: ItemRow) => {
+                if (!isNaN(parseFloat(b.price))) {
+                  return a + parseFloat(b.price);
+                } else {
+                  return a;
+                }
+              }, 0),
+            options: {
+              separation: false,
+            },
+          },
+        ]),
     };
-    // A4 595.28 x 841.89 (portrait) (about width sizes)
-    // width
-    await doc.table(table, {
+    if (data.itemRows.length < 15) {
+      table.datas[table.datas.length - 2].price += Array(Math.max(1200 - 60 * data.itemRows.length, 0)) //hack to increase padding of last row
+        .fill(" ")
+        .join("");
+    } else {
+      table.datas.splice(table.datas.length - 2, 1);
+    }
+    table.datas[table.datas.length - 2].options = { separation: true, padding: [0, 0, 0, 100] }; //property can't exist on other rows
+
+    const options = {
       x: 70,
-      y: data.fields.destAddress ? 310 : 260,
+      y: data.fields.destAddress ? 250 : 200,
       columnsSize: [doc.page.width - 240, 100],
-    });
-    // or columnsSize
-    // done!
+    } as any;
+
+    await doc.table(table, options);
+
+    const startX = doc.x;
+    const startY = doc.y;
+    if (data.itemRows.length < 25) {
+      //if its longer then it will go to the next page and the lines wont look nice.
+      doc
+        .lineWidth(0.5)
+        .moveTo(startX, startY - 10)
+        .lineTo(startX, (data.fields.destAddress ? 250 : 200) - 4)
+        .lineTo(doc.page.width - 70, (data.fields.destAddress ? 250 : 200) - 4)
+        .lineTo(doc.page.width - 70, startY - 10)
+        .stroke()
+        .moveTo(doc.page.width - 170, startY - 10)
+        .lineTo(doc.page.width - 170, (data.fields.destAddress ? 250 : 200) - 4)
+        .stroke();
+    }
     doc.end();
   })();
 }
