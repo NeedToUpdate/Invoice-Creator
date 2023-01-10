36203f8d530591c10efd9e8d115ed564c30b4f1c
Artem <updatethatpl0x@gmail.com>
2023-01-09 19:59:37 -0500

    added mongo to project

--- mongo-entrypoint/mongo-init.sh
+++ mongo-entrypoint/mongo-init.sh
@@ -1,3 +1,3 @@
 #creates a non-root user so we dont use root priviledges with our app just in case
-mongosh admin --host localhost -u root -p pass --eval "db.createUser({user: '$MONGODB_USERNAME', pwd: '$MONGODB_PASSWORD', roles: [{role: 'readWrite', db: '$MONGODB_DATABASE'}]});"
+mongosh admin --host localhost -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --eval "db.createUser({user: '$MONGODB_USERNAME', pwd: '$MONGODB_PASSWORD', roles: [{role: 'readWrite', db: '$MONGODB_DATABASE'}]});"
 echo "Mongo users created."
